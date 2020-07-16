0a9cfebb80327d046f4a385cbc047404599e33e8
yuzh0816 <46953451+yuzh0816@users.noreply.github.com>
2020-07-16 11:27:10 +0800

    Update writetohosts.bat

--- writetohosts.bat
+++ writetohosts.bat
@@ -1,4 +1,6 @@
 @echo off
-set /p ip=请输入ip地址 
-set /p host=请输入域名 
+echo "The batch file will use the administrative privilege"
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+set /p ip=Please enter the IP address
+set /p host=Please enter the domain
 ECHO ^ip host>>%WINDIR%\system32\drivers\etc\hosts
